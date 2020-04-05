d70e9c82322c2ebbe3c183c077a31990b0f652f1
umesh47 <071bel2014@gmail.com>
2020-04-05 14:26:07 +0545

    complete MTV pattern

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
 
 def welcome(request):
-    return render(request, "website/welcome.html",{"message": "THis data was sent from views","x":"demo from views"})
+    return render(request, "website/welcome.html",{"num_meetings": Meeting.objects.count()})
 
 
 def date(request):
