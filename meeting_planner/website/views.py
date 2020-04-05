1e360326e1e4789d42a949c7b7c81616b210df11
umesh47 <071bel2014@gmail.com>
2020-04-05 20:54:07 +0545

    template with for loop

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -5,7 +5,7 @@ from meetings.models import Meeting
 
 
 def welcome(request):
-    return render(request, "website/welcome.html",{"num_meetings": Meeting.objects.count()})
+    return render(request, "website/welcome.html",{"meetings": Meeting.objects.all()})
 
 
 def date(request):
