876f1d48e02e6301f796787ff7e65ac267678d13
umesh47 <071bel2014@gmail.com>
2020-04-06 15:34:55 +0545

    template with a modelform

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.detail, name='detail'),
     path('rooms', views.rooms_list, name='rooms'),
+    path('new', views.new, name='new'),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
+
 from .models import Meeting,Room
+
 
 def detail(request,id):
     meeting= get_object_or_404(Meeting, pk=id)
@@ -9,4 +12,9 @@ def rooms_list(request):
     return render(request, 'meetings/rooms_list.html',{'rooms':Room.objects.all()})
 
 
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    form = MeetingForm()
+    return render(request, 'meetings/new.html',{'form':form})
 
