147738b5e252209fe492c4f27eeed6a83b1c9a90
umesh47 <071bel2014@gmail.com>
2020-04-06 15:58:52 +0545

    processing user input

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 
 from .models import Meeting,Room
@@ -13,8 +13,13 @@ def rooms_list(request):
 
 
 MeetingForm = modelform_factory(Meeting, exclude=[])
-
 def new(request):
-    form = MeetingForm()
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
     return render(request, 'meetings/new.html',{'form':form})
 
